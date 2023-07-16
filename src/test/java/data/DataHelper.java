8610dd892da9ee353cb16b5ed04c1666e34209ca
Anastasiia <anastasia994@yandex.ru>
2023-07-16 09:39:36 +0500

    fixed tests

--- src/test/java/data/DataHelper.java
+++ src/test/java/data/DataHelper.java
@@ -1,39 +1,64 @@
 package data;
 
 import com.github.javafaker.Faker;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import lombok.Value;
 
 import java.util.Locale;
 
 public class DataHelper {
+
     private static Faker faker = new Faker(new Locale("en"));
 
     private DataHelper() {
-
     }
 
     public static AuthInfo getAuthInfoWithTestData() {
         return new AuthInfo("vasya", "qwerty123");
     }
 
-    private static String generateRandomLogin() {
-        return faker.internet().username();
+    public static String generateRandomLogin() {
+        return faker.name().username();
     }
 
     private static String generateRandomPassword() {
         return faker.internet().password();
     }
 
-    private static String generateRandomUser() {
+    public static AuthInfo generateRandomUser() {
         return new AuthInfo(generateRandomLogin(), generateRandomPassword());
     }
 
-    public static VerificationCode generateRendomVerificationCode() {
-        return new VerificationCode(faker.nummerify("######"));
+    public static VerificationCode generateRandomVerificationCode() {
+        return new VerificationCode(faker.numerify("######"));
     }
+
 
     @Value
     public static class AuthInfo {
-
+        String login;
+        String password;
     }
+
+    @Value
+    public static class VerificationCode {
+        String code;
+    }
+
+    @Data
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class AuthCode {
+        private String id;
+        private String user_id;
+        private String code;
+        private String created;
+    }
+
+    public static AuthInfo getIncorrectPassword() {
+        return new AuthInfo("petya", "123qwerty");
+    }
+
 }
